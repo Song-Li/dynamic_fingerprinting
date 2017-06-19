3d5906a10a5464f0a4d235b7f8c6560d02d1d0ce
Song-Li <lisong1013@gmail.com>
2017-06-19 11:30:12 -0400

    doing analyze

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -2,6 +2,8 @@ import MySQLdb
 import ConfigParser, os
 
 config = ConfigParser.ConfigParser()
+config.read('password.ignore')
+print config.get('mysql', 'username')
 db = MySQLdb.connect(host='localhost',
                      user=user_name,
                      passwd = password,
