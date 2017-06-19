101fdcdfe319da9ff94346c076bf4e6fb0cd3255
Song-Li <lisong1013@gmail.com>
2017-06-19 13:17:43 -0400

    finished the analyze of gpuimgs

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -1,9 +1,35 @@
 from database import Database
 class Analyzer():
-    def __init__(self):
-        self.db = Database('uniquemachine')
-        self.cols = self.db.run_sql("SHOW COLUMNS FROM features")
+    ignores = ['id', 'time', 'browser_fingerprint', 'computer_fingerprint_1']
+    db = Database('uniquemachine')
+    cols = db.run_sql("SHOW COLUMNS FROM features")
 
+    def __init__(self):
+        pass
+
+    def check_imgs_difference_dy_id(self, str_1, str_2):
+        """
+        check the differences of two gpu rendering result strs
+        vars: str_1, str_2
+        return: the differences
+        """
+        imgs_1 = str_1.split(',')
+        imgs_2 = str_2.split(',')
+        length = len(imgs_1)
+        if len(imgs_2) != length:
+            return "different number of imgs"
+        imgs_1 = sorted(imgs_1, key=lambda img: int(img.split('_')[0]))
+        imgs_2 = sorted(imgs_2, key=lambda img: int(img.split('_')[0]))
+
+        res = {}
+        for i in range(length):
+            img_1 = imgs_1[i].split('_')[2]
+            img_2 = imgs_2[i].split('_')[2]
+            if img_1 != img_2:
+                res[i] = (img_1, img_2)
+        return res
+
+            
     def check_difference_by_id(self, base_id, entry_id):
         """
         check the difference of two entries based on the ids
@@ -14,13 +40,19 @@ class Analyzer():
         compare_entry = self.db.get_entry_by_id('features', entry_id)
         length = len(base_entry)
         for i in range(length):
+            if self.cols[i][0] in self.ignores:
+                continue
             if base_entry[i] != compare_entry[i]:
-                print self.cols[i][0], base_entry[i]
+                if self.cols[i][0] == 'gpuimgs':
+                    diff = self.check_imgs_difference_dy_id(base_entry[i], compare_entry[i])
+                    print self.cols[i][0], diff
+                else:
+                    print self.cols[i][0]
         
 
 def main():
     analyzer = Analyzer()
-    analyzer.check_difference_by_id(2, 1)
+    analyzer.check_difference_by_id(12, 1)
 
 
 
