924de300f04c08f71307dcc2d7e7c2d4d90e3ce2
Song-Li <lisong1013@gmail.com>
2017-08-25 13:24:47 -0400

    more function download exe

--- analyze/downloadExe.py
+++ analyze/downloadExe.py
@@ -2,8 +2,11 @@ import urllib2
 import urllib
 import re
 import json
+import time
 
-for i in range(1,1213):
+size = 0
+start = time.time()
+for i in range(2,1213):
     response = urllib2.urlopen('http://rj.baidu.com/soft/lists/0/' + str(i))
     html = response.read()
     p = re.compile(r' {"data(.*)}};')
@@ -11,10 +14,18 @@ for i in range(1,1213):
     json_str = p.search(html).group(0)[0:-1]
     softwares = json.loads(json_str)['data']['softList']
     for software in softwares['list']:
+        print "Speed: " + "%.2f" % (size / (time.time() - start)) + "M/s. " + str(size) + "M of data downloaded Time: " + str(int(time.time() - start)) + "s"
         download_url = software['url']
-        print "downloading..." + download_url
         if download_url.find(".exe") == -1:
             continue
         software_name = download_url[download_url.rfind('/') + 1:download_url.rfind('.exe') + 4]
         download_file = urllib.URLopener()
-        download_file.retrieve(download_url, "./downloaded/" + software_name)
+        length = urllib.urlopen(download_url).info().getheaders("Content-Length")[0]
+        this_size = int(length) / 1024 / 1024
+        size += this_size
+        print "Downloading..." + download_url + " Size: " + str(this_size) + "M"
+        try:
+            download_file.retrieve(download_url, "./downloaded/" + software_name)
+        except:
+            print "error: ", download_url
+
