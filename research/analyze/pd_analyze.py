4f7799910059ec9f1a472a401c8a6520d84ed2c9
Song-Li <lisong1013@gmail.com>
2017-11-05 13:41:47 -0500

    ana two features table

--- research/analyze/pd_analyze.py
+++ research/analyze/pd_analyze.py
@@ -2,19 +2,36 @@ import pandas as pd
 import datetime
 from database import Database
 
+
+def featureDiff(f1, f2):
+    return f1 != f2 and 'None' not in str(f1) and 'None' not in str(f2) 
+
 db = Database('uniquemachine')
 df = pd.read_sql('select * from features;', con=db.get_db())    
+print ("data loaded")
 
 cookies = df.groupby('label')
-
-length = 0
-more_than_2 = 0
+feature_names = list(df.columns.values)
+numbers = {}
 total = 0
+more_than_2 = 0
+stop = False 
+res_table = {}
+
+def printTable(table):
+    head = [' '] + feature_names
+    print ' '.join(['{:<5.5}'.format(name) for name in head])
+    for k in feature_names:
+        if k not in table:
+            continue
+        print '{:<5.5} '.format(k) + ' '.join(['{:<5.5}'.format(str(table[k][k2])) for k2 in feature_names])
+
+
 for key, items in cookies:
-    if items['browser_fingerprint'].nunique() > 1:
+    if items['browserfingerprint'].nunique() > 1:
         total += 1
         min_time = datetime.datetime.now()
-        max_time = datetime.datetime.now() - datetime.timedelta(days = 100)
+        max_time = datetime.datetime.now() - datetime.timedelta(days = 10000)
         for index, row in items.iterrows():
             if min_time > row['time']:
                 min_time = row['time']
@@ -22,16 +39,19 @@ for key, items in cookies:
             if max_time < row['time']:
                 max_time = row['time']
                 max_row = row
-        if max_time - min_time > datetime.timedelta(days = 2):
+        if max_time - min_time > datetime.timedelta(days = 0):
             more_than_2 += 1
-            if min_row['gpuimgs'] == max_row['gpuimgs']:
-                length += 1
+            for k in feature_names:
+                if not featureDiff(min_row[k], max_row[k]):
+                    continue
+                if k not in numbers:
+                    numbers[k] = {} 
+                for k2 in feature_names:
+                    if k2 not in numbers[k]:
+                        numbers[k][k2] = 0
+                    if featureDiff(min_row[k2], max_row[k2]):
+                        numbers[k][k2] += 1
 
-#    if min_row['gpuimgs'] == max_row['gpuimgs']:
-#        print min_row['gpuimgs']
-#        print max_row['gpuimgs']
-#        print "======================================================="
 
-print length
-print more_than_2
-print total
+printTable(numbers)
+print ("total", total)
