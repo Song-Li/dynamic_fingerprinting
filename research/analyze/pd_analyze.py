3d5337465107230a549dab5a7307bd33274787e9
guoliLU <gul317@lehigh.edu>
2017-11-10 15:35:31 -0500

    updating analyze

--- research/analyze/pd_analyze.py
+++ research/analyze/pd_analyze.py
@@ -12,12 +12,8 @@ print ("data loaded")
 
 cookies = df.groupby('label')
 feature_names = list(df.columns.values)
-numbers = {}
-total = 0
-more_than_2 = 0
-stop = False 
-res_table = {}
 
+# print a 2D table
 def printTable(table):
     head = [' '] + feature_names
     print ' '.join(['{:<5.5}'.format(name) for name in head])
@@ -26,32 +22,48 @@ def printTable(table):
             continue
         print '{:<5.5} '.format(k) + ' '.join(['{:<5.5}'.format(str(table[k][k2])) for k2 in feature_names])
 
+# get the both-change number of features
+def relation(cookies):
+    numbers = {}
+    total = 0
+    more_than_2 = 0
+    stop = False 
+    res_table = {}
+    for key, items in cookies:
+        if items['browserfingerprint'].nunique() > 1:
+            total += 1
+            min_time = datetime.datetime.now()
+            max_time = datetime.datetime.now() - datetime.timedelta(days = 10000)
+            for index, row in items.iterrows():
+                if min_time > row['time']:
+                    min_time = row['time']
+                    min_row = row
+                if max_time < row['time']:
+                    max_time = row['time']
+                    max_row = row
+            if max_time - min_time > datetime.timedelta(days = 0):
+                more_than_2 += 1
+                for k in feature_names:
+                    if not featureDiff(min_row[k], max_row[k]):
+                        continue
+                    if k not in numbers:
+                        numbers[k] = {} 
+                    for k2 in feature_names:
+                        if k2 not in numbers[k]:
+                            numbers[k][k2] = 0
+                        if featureDiff(min_row[k2], max_row[k2]):
+                            numbers[k][k2] += 1
+    return numbers
 
-for key, items in cookies:
-    if items['browserfingerprint'].nunique() > 1:
-        total += 1
-        min_time = datetime.datetime.now()
-        max_time = datetime.datetime.now() - datetime.timedelta(days = 10000)
-        for index, row in items.iterrows():
-            if min_time > row['time']:
-                min_time = row['time']
-                min_row = row
-            if max_time < row['time']:
-                max_time = row['time']
-                max_row = row
-        if max_time - min_time > datetime.timedelta(days = 0):
-            more_than_2 += 1
-            for k in feature_names:
-                if not featureDiff(min_row[k], max_row[k]):
-                    continue
-                if k not in numbers:
-                    numbers[k] = {} 
-                for k2 in feature_names:
-                    if k2 not in numbers[k]:
-                        numbers[k][k2] = 0
-                    if featureDiff(min_row[k2], max_row[k2]):
-                        numbers[k][k2] += 1
+# get how many users have different fingerprints
+def diff_diff(cookies):
+    for key, items in cookies:
+        
 
 
+
+
+
+
+numbers = relation(cookies)
 printTable(numbers)
-print ("total", total)
