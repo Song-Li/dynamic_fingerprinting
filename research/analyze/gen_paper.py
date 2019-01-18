df9551f651c43879fa20ac2ac8eb1a5f5dd9a3a3
Song-Li <lisong1013@gmail.com>
2019-01-18 21:13:53 +0000

    updating value by date

--- research/analyze/gen_paper.py
+++ research/analyze/gen_paper.py
@@ -7,8 +7,9 @@ from fonts import Fonts
 def main():
     db = Database('forpaper345') 
     paperlib = Paperlib(db)
+    paperlib.value_change_by_date('plugins', 'Google')
     #paperlib.feature_change_by_date_paper('jsFonts')
-    paperlib.relation_detection_os_browser()
+    #paperlib.relation_detection_os_browser()
     #df = db.load_data(table_name = 'allchanges')
     #paperlib.get_flip_list(df)
     #paperlib.relation_detection_os_browser(df = df)
